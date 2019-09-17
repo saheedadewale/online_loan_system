c3afeaf411fe3582ea6d560853c697935cb5adb3
epsalumTech <o.adewale17@gmail.com>
2019-09-17 06:15:17 -0700

    Modify styles, signup, formprocess and db.json file

--- js/formprocess.js
+++ js/formprocess.js
@@ -1,6 +1,52 @@
 $(document).ready(function(){
     
-    $('.createAcct').click(function(){
-        alert('Hello');
+    $('#createAcct').click(function(){
+        const surname = $('#surname').val();
+        const othername = $('#othername').val();
+        const email = $('#email').val();
+        const phoneno = $('#phoneno').val();
+        const bvn = $('#bvn').val();
+        //prevent epty input
+      if(!surname || !othername|| !email || !phoneno || !bvn){
+          $('.msg').html('Kindly fill all the required fields');
+      }else{
+        $('.msg').html(''); 
+      }
+
+        //Make get request to the endpoint, to check if user already exist
+    $.ajax({
+        method: 'GET',
+        url: `http://localhost:3004/userstable?email=${email}`,
+        data: {
+          email,
+        },
+        // beforeSend: function() {
+        //   $('.regMessage').html('Please Waite While Loading...');
+        // },
+        success: function(response) {
+          if (response.length) {
+            $('.regMessage').html('User already exist');
+          } else {
+            //Submit the usertable
+            $.ajax({
+              method: 'POST',
+              url: 'http://localhost:3004/userstable',
+              data: {
+                surname,
+                othername,
+                email,
+                phoneno,
+                bvn,
+              },
+              beforeSend: function() {
+                $('.regMessage').html('Please Waite While Loading...');
+              },
+              success: function() {
+                $('.regMessage').html('Account Created Successfull');
+              },
+            });
+          }
+        },
+      });
     });
 });
