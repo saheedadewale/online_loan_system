11151220b0208756ae5d78c7646f8ac164e9abbc
epsalumTech <o.adewale17@gmail.com>
2019-09-18 01:54:54 -0700

    Write jas code to submit loan application

--- js/formprocess.js
+++ js/formprocess.js
@@ -55,4 +55,46 @@ $(document).ready(function(){
         },
       });
     });
+
+    //loan Application form process
+    $('#apply').click(function(event){
+      //getting value from the users
+      event.preventDefault();
+      const employer = $('#employer').val();
+      const state = $('#state').val();
+      const bank = $('#bank').val();
+      const acctno = $('#acctno').val();
+      const netsalary = $('#netsalary').val();
+      const eligibleamt = $('#eligibleamt').val();
+      const lesseramt = $('#lesseramt').val();
+      const tenor = $('#tenor').val();
+
+      //prevent user empty submission
+      if(!employer || !bank || !acctno || !netsalary || !eligibleamt || ! tenor){
+        alert("Field Left Unsupply");
+        return false;
+      }else{
+         //making post request to the loan endpoint
+        $.ajax({
+          method: 'POST',
+          url: 'http://localhost:3004/loantable', 
+          data: {
+            employer,
+            state,
+            bank,
+            acctno,
+            netsalary,
+            eligibleamt,
+            lesseramt,
+            tenor,
+          },
+          beforeSend: function() {
+            $('.loanMessage').html('Please Waite While Loading...');
+          },
+          success: function() {
+            $('.loanMessage').html('You Have Successfully Apply for loan');
+          },
+        });
+      }
+    });
 });
