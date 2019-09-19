7a932045b83db07dcbd500e2c4665871b100423e
epsalumTech <o.adewale17@gmail.com>
2019-09-18 19:24:10 -0700

    Modify

--- js/formprocess.js
+++ js/formprocess.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
+  $('.loginWrapper').hide()
     //hide login form
     $('.loginLink').click(function(event){
         event.preventDefault();
-       $('.loginFormWrapper').hide();
+       $('.signupWrapper').hide();
        $('.loginWrapper').fadeIn();
     });
+
+    $('.signuplink').click(function(event){
+      event.preventDefault();
+     $('.signupWrapper').fadeIn();
+     $('.loginWrapper').hide();
+  });
+
     $('#createAcct').click(function(event){
         event.preventDefault();
         const surname = $('#surname').val();
@@ -15,6 +23,7 @@ $(document).ready(function(){
         //prevent epty input
       if(!surname || !othername|| !email || !phoneno || !bvn){
           $('.msg').html('Kindly fill all the required fields');
+          return false;
       }else{
         $('.msg').html(''); 
       }
@@ -55,4 +64,39 @@ $(document).ready(function(){
         },
       });
     });
+
+    //Login Function
+  $('#login').click(function(event) {
+    event.preventDefault();
+    const emailLog = $('#emailLog').val();
+    const phonenoLog = $('#phonenoLog').val();
+    if (!emailLog || !phonenoLog) {
+      $('.errLogin').html('Kindly fill in all fields');
+      return;
+    }
+    //Check if the user is in the database
+    $.ajax({
+      method: 'GET',
+      url: `http://localhost:3004/userstable?email=${emailLog}&phoneno=${phonenoLog}`,
+      data: {
+        email: emailLog,
+        phoneno: phonenoLog,
+      },
+      beforeSend: function() {
+        $('.loginSuccess').html('Loading....');
+      },
+      success: function(response) {
+        if (response.length) {
+          $('.loginSuccess').html('Login sucessful');
+         // $('.checkLogin').html('You are logged in');
+          localStorage.setItem('email', emailLog);
+          //redirect to home page if the login is successfull
+          window.location.assign('dashboard.html');
+        } else {
+          $('.loginSuccess').html('Invalid Login Parameters');
+        }
+      },
+    });
+  });
+
 });
